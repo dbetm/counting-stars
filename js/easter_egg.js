6de502f84ad4f192fbb09b2e07adc59e78a6a047
dbetm <davbetm@gmail.com>
2023-11-30 00:24:47 -0600

    refactor: update easter egg

--- js/easter_egg.js
+++ js/easter_egg.js
@@ -7,22 +7,9 @@ function trick() {
     count += 1;
     counterEeg.innerHTML = count;
 
-    if(count == 4) {
-        emoji.innerHTML = "❤️";
-    }
-    else if(count == 6) {
-        emoji.innerHTML = "🌖";
-    }
-    else if (count == 7) {
-        emoji.innerHTML = "🍀";
-    }
-    else if (count == 13) {
-        emoji.innerHTML = "🌻";
-    }
-    else if (count == 17) {
+    if(count == 43) {
         count = 0;
         emoji.innerHTML = "⭐";
         counterEeg.innerHTML = "+)";
-        window.open("https://youtu.be/n9ta1Pgeu80", "_blank");
     }
 }
